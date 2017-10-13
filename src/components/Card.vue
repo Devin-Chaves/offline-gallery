d0c1a6a2779eac039bf63901af274165d96cacf4
Devin Chaves <dchav90@gmail.com>
2017-10-13 19:05:51 -0400

    template

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,11 @@
 <template lang="html">
-
+  <div class="card">
+    <div class="card-content">
+      <img :src="collection.imageUrl" :alt="collection.collector">
+      <h4></h4>
+      <p></p>s
+    </div>
+  </div>
 </template>
 
 <script>
